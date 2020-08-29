258b6122381ae3f6401d377e0451f4b3cdbc0438
thevency <46436668+thevency@users.noreply.github.com>
2020-08-29 21:30:24 +0700

    Version 1.0
    simple to combine cucumber and serenity screenplay

---
+++ src/test/java/CucumberTestSuite.java
@@ -0,0 +1,14 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features ="src/test/resources/"
+)
+public class CucumberTestSuite {
+
+
+}
